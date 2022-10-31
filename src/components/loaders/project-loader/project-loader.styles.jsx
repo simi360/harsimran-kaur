42f16faafd90fe0a20f3caa63bee602bdcc096d1
Harsimran Kaur <infamously13@gmail.com>
2022-10-31 17:42:55 -0400

    feat: Adding project-loader component

---
+++ src/components/loaders/project-loader/project-loader.styles.jsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import { cubeAnimation } from "../../../utils/kreyframes";
+import { FadeUp } from "../../../utils/animations";
+
+const LoaderContainer = styled.section`
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #fff;
+`;
+
+const Cube = styled.div`
+  width: 10rem;
+  height: 10rem;
+  margin: 0 auto;
+  animation: ${cubeAnimation} 1s infinite ${(p) => p.theme.animations.easeInOut};
+  /* Animations */
+  ${FadeUp}
+`;
+
+const P = styled.p`
+  margin-top: 2rem;
+  font-size: 3rem;
+  line-height: 1;
+  font-family: ${(p) => p.theme.font.poppins};
+  color: ${(p) => p.theme.color.primary};
+  text-align: center;
+  /* Animations */
+  ${FadeUp}
+`;
+
+export { LoaderContainer, Cube, P };
