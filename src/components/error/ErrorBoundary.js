bd75c051d8fef6462c4bb18a6aa82b50c607edcf
Harsimran Kaur <infamously13@gmail.com>
2022-11-01 19:04:11 -0400

    Adding images and setting up error boundary

---
+++ src/components/error/ErrorBoundary.js
@@ -0,0 +1,45 @@
+import React, { Suspense, lazy } from "react";
+import PropTypes from "prop-types";
+
+const ErrorSlider = lazy(() => import("./error-slider/ErrorSlider"));
+import ErrorDefault from './ErrorDefault';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    // eslint-disable-next-line no-console
+    console.error("ErrorBoundary caught an error ", error, info);
+  }
+
+  render() {
+    const { type } = this.props;
+    if (this.state.hasError) {
+      switch (type) {
+        case "slider":
+          return (
+            <Suspense fallback={<div>Chargement...</div>}>
+              <ErrorSlider />
+            </Suspense>
+          );
+        default:
+          return <ErrorDefault />;
+      }
+    }
+
+    return this.props.children;
+  }
+}
+
+ErrorBoundary.propTypes = {
+  type: PropTypes.string,
+};
+
+export default ErrorBoundary;
