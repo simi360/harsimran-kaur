cbdc0b9c6906d25c340cf305313c5352c0150f26
Harsimran Kaur <infamously13@gmail.com>
2022-11-01 14:24:45 -0400

    Adding Carret icon and slider-button component

---
+++ src/components/slider/slider-button/slider-button.component.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Carret from "../../icons/Carret";
+import SliderButtonStyles from "./slider-button.styles";
+
+const SliderButton = ({
+  onClickHandler,
+  hasFollowingSlide,
+  label,
+  isPrev,
+  colors,
+}) => {
+  let enableArrow =
+    typeof hasFollowingSlide == "boolean"
+      ? hasFollowingSlide
+      : hasFollowingSlide(isPrev);
+
+  return (
+    <SliderButtonStyles
+      type="button"
+      onClick={onClickHandler}
+      title={label}
+      className={`control-arrow ${isPrev ? "control-prev" : "control-next"} ${
+        enableArrow ? "" : "control-disabled"
+      }`}
+    >
+      <Carret colors={colors} />
+    </SliderButtonStyles>
+  );
+};
+
+SliderButton.propTypes = {
+  onClickHandler: PropTypes.func.isRequired,
+  hasFollowingSlide: PropTypes.oneOfType([PropTypes.func, PropTypes.bool]),
+  label: PropTypes.string.isRequired,
+  isPrev: PropTypes.bool.isRequired,
+  colors: PropTypes.object.isRequired,
+};
+
+export default SliderButton;
---
+++ src/components/slider/slider-button/slider-button.styles.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+const SliderButtonStyles = styled.button`
+  :focus {
+    outline: none;
+  }
+  /* double & needed to override react-responsive-carousel styles */
+  &&.control-prev,
+  &&.control-next {
+    svg {
+      width: 0.75rem;
+    }
+    &.control-disabled {
+      opacity: 0.4;
+      display: inline-block;
+    }
+  }
+  &&.control-prev {
+    svg {
+      transform: scaleX(-1);
+    }
+  }
+  @media (min-width: ${(p) => `${p.theme.bp.tablets}px`}) {
+    padding: 1rem 1rem 0;
+  }
+`;
+
+export default SliderButtonStyles;
